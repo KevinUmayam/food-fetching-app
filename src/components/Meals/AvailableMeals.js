da968329773180541e325e33b2031dabe5a25cfa
Kevin Umayam <kevin.m.umayam@gmail.com>
2023-03-15 14:04:58 -0700

    first commit plus connection to the firebase database

---
+++ src/components/Meals/AvailableMeals.js
@@ -0,0 +1,33 @@
+import Card from '../UI/Card';
+import MealItem from './MealItem/MealItem';
+import classes from './AvailableMeals.module.css';
+import { useEffect } from 'react';
+
+
+const AvailableMeals = () => {
+  useEffect(() => {
+    const fetchmeals = async () => {
+      await fetch('https://food-fetching-db-default-rtdb.firebaseio.com/meals.json')
+    }
+    fetchmeals()
+  }, [])
+  const mealsList = DUMMY_MEALS.map((meal) => (
+    <MealItem
+      key={meal.id}
+      id={meal.id}
+      name={meal.name}
+      description={meal.description}
+      price={meal.price}
+    />
+  ));
+
+  return (
+    <section className={classes.meals}>
+      <Card>
+        <ul>{mealsList}</ul>
+      </Card>
+    </section>
+  );
+};
+
+export default AvailableMeals;
